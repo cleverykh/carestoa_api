2f62ac7ed47c5762fa74930a4757b9c9eb1ebe24
david.yang <cleverykh@gmail.com>
2022-03-07 19:19:19 +0900

    기본 라우팅 삭제

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,7 @@
-import { Request } from 'express';
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/clever')
-  getClever(@Req() req: Request): string {
-    console.log(req);
-    return this.appService.getClever();
-  }
 }
