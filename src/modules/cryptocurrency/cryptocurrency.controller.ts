8f17d4aca88e8350a8c59c57181ce5be50555c44
david.yang <cleverykh@gmail.com>
2022-03-17 17:14:47 +0900

    암호화폐 api 의 summary 누락 추가

--- src/modules/cryptocurrency/cryptocurrency.controller.ts
+++ src/modules/cryptocurrency/cryptocurrency.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PaginatedResponse } from 'src/common';
 import { CryptocurrencyService } from './cryptocurrency.service';
 import { CreateCryptocurrencyDto } from './dto/create-cryptocurrency.dto';
@@ -11,6 +11,7 @@ export class CryptocurrencyController {
   constructor(private readonly cryptocurrencyService: CryptocurrencyService) {}
 
   @Post()
+  @ApiOperation({ summary: '암호화폐 생성' })
   async cryptocurrencyCreate(
     @Body() createCryptocurrencyDto: CreateCryptocurrencyDto,
   ): Promise<Cryptocurrency> {
@@ -18,6 +19,7 @@ export class CryptocurrencyController {
   }
 
   @Get()
+  @ApiOperation({ summary: '암호화폐 조회' })
   async cryptocurrencyInquiry(): Promise<PaginatedResponse<Cryptocurrency>> {
     return await this.cryptocurrencyService.findAll();
   }
