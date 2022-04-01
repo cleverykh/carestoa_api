77ba1c894ab5c5915e27c2f913a86ac47ec66938
david.yang <cleverykh@gmail.com>
2022-04-01 17:44:27 +0900

    Contract UPDATE 완료

--- src/modules/contract/contract.module.ts
+++ src/modules/contract/contract.module.ts
@@ -6,8 +6,12 @@ import { Contract } from './entities/contract.entity';
 import { User } from '../users/entities/user.entity';
 import { CallHttpService } from 'src/core';
 import { HttpModule } from '@nestjs/axios';
+import { ContractExchangeMapper } from '../contract_exchange_mapper/contract_exchange_mapper.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Contract, User]), HttpModule],
+  imports: [
+    TypeOrmModule.forFeature([Contract, User, ContractExchangeMapper]),
+    HttpModule,
+  ],
   controllers: [ContractController],
   providers: [ContractService, CallHttpService],
 })
--- src/modules/contract/contract.service.ts
+++ src/modules/contract/contract.service.ts
@@ -1,16 +1,23 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, UpdateQueryBuilder, UpdateResult } from 'typeorm';
-import { User } from '../users/entities/user.entity';
+import { getConnection, Repository, UpdateResult } from 'typeorm';
 import { CreateContractDto } from './dto/create-contract.dto';
 import { UpdateContractDto } from './dto/update-contract.dto';
+import { User } from '../users/entities/user.entity';
 import { Contract } from './entities/contract.entity';
+import { ContractExchangeMapper } from '../contract_exchange_mapper/contract_exchange_mapper.entity';
 
 @Injectable()
 export class ContractService {
   constructor(
     @InjectRepository(Contract)
     private readonly contractRepo: Repository<Contract>,
+    @InjectRepository(ContractExchangeMapper)
+    private readonly contractExchangeMapperRepo: Repository<ContractExchangeMapper>,
   ) {}
 
   /**
@@ -67,15 +74,73 @@ export class ContractService {
     contractNo: number,
     updateContractDto: UpdateContractDto,
   ): Promise<UpdateResult> {
-    console.log(updateContractDto);
-    let contract = await this.contractRepo
-      .createQueryBuilder()
-      .update()
-      .set(updateContractDto)
-      .where('no = :contractNo', { contractNo })
-      .execute();
+    //contract and authority check
+    const checkExist = await this.contractRepo.findOne({
+      where: {
+        no: contractNo,
+      },
+    });
 
-    return contract;
+    if (!checkExist) {
+      throw new BadRequestException({
+        message: 'This contract does not exist.',
+      });
+    } else if (checkExist.userNo !== userInfo.no) {
+      throw new UnauthorizedException({
+        message: `You don't have the authority.`,
+        error: 401,
+      });
+    }
+
+    //transaction start
+    const queryRunner = await getConnection().createQueryRunner();
+    await queryRunner.startTransaction();
+    try {
+      //update for exchanges
+      const contractExchangeMapperNumberArray: any = [];
+      if (updateContractDto.exchanges.length > 0) {
+        updateContractDto.exchanges.map((val) => {
+          contractExchangeMapperNumberArray.push({
+            exchangeNo: val,
+            contractNo,
+          });
+        });
+
+        // delete
+        await this.contractExchangeMapperRepo.delete({ contractNo });
+
+        //insert
+        await this.contractExchangeMapperRepo
+          .createQueryBuilder()
+          .insert()
+          .into(ContractExchangeMapper)
+          .values(contractExchangeMapperNumberArray)
+          .updateEntity(false)
+          .execute();
+
+        delete updateContractDto.exchanges;
+      }
+
+      //update for contract
+      let contract = await this.contractRepo
+        .createQueryBuilder()
+        .update()
+        .set(updateContractDto)
+        .where('no = :contractNo', { contractNo })
+        .execute();
+
+      await queryRunner.commitTransaction();
+
+      return contract;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+
+      throw new BadRequestException({
+        message: 'Bad request.',
+      });
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   // findAll() {
